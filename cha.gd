76d2af34d68267f8db5c81b12d99b94244d3d321
KevChhhh <1730246495@qq.com>
2024-02-22 15:08:37 -0500

    moving

--- cha.gd
+++ cha.gd
@@ -14,12 +14,12 @@ var x = 1
 @export var speed = 5
 # Called every frame. 
 func _process(delta):
-	if Input.is_action_pressed('move_up'):
+	if Input.is_action_pressed("up"):
 		position.y -= speed
-	if Input.is_action_pressed('move_down'):
-		position.y -= speed
-	if Input.is_action_pressed('move_up'):
-		position.y -= speed
-	if Input.is_action_pressed('move_up'):
-		position.y -= speed
+	if Input.is_action_pressed('down'):
+		position.y += speed
+	if Input.is_action_pressed('right'):
+		position.x += speed
+	if Input.is_action_pressed('left'):
+		position.x -= speed
 	pass
